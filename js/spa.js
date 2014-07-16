71a15f0069b840fb22fb89fb2275a623dab11b79
Kenji Okamoto <kokamoto@mail.ccsf.edu>
2014-07-15 18:16:11 -0700

    finish up to 5.5

--- js/spa.js
+++ js/spa.js
@@ -14,7 +14,9 @@
 
 /* jshint -W079 */
 var spa = (function(){
+  'use strict';
     var initModule = function($container){
+        spa.model.initModule();
         spa.shell.initModule($container);
     };
     return {initModule: initModule};
